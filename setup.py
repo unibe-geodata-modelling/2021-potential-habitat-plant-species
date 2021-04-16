3caccfa1d4065920d7e4f13f1c4a3d5b9483bd06
Ivana <ivana.cervenka@gmail.com>
2021-04-16 14:58:03 +0200

    .

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name='2021-potential-habitat-plant-species',
+    version='',
+    packages=[''],
+    url='',
+    license='',
+    author='Ivana',
+    author_email='',
+    description=''
+)
+
+
